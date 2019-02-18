92acc37ae9ee9b2e3e6ff50b7923f20e35e6e974
Lumir Balhar <lbalhar@redhat.com>
2019-02-18 20:09:57 +0100

    Pridana prvni verze vtipniku, ktera umi vypsat vsechny vtipy

---
+++ vtipnik.py
@@ -0,0 +1,26 @@
+import sys
+from glob import glob
+
+
+def nacti_vtip(cesta_ke_vtipu):
+    """
+    Když této funkci dáš jméno textového souboru ze složky vtipy,
+    tak vrátí text vtipu jako řetězec.
+    """
+    try:
+        with open(cesta_ke_vtipu, 'r') as f:
+            return f.read()
+    except FileNotFoundError:
+        sys.exit('Bohuzel soubor s vtipem neexistuje: {}'.format(cesta_ke_vtipu))
+
+
+def seznam_vtipu():
+    """
+    Funkce načtě obsah složky a vrátí seznam všech dostupných vtipů
+    """
+    return glob("vtipy/*")
+
+
+for vtip in seznam_vtipu():
+    print("=" * 80)
+    print(nacti_vtip(vtip))
